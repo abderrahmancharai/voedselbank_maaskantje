eb522bcd48b638005507b78b7f7cb2aabfde2b05
KaganAkdeniz <kaganakdeniz2004@gmail.com>
2023-06-20 14:47:26 +0200

    controller kleine bugs gefixed

--- app/controllers/VoedselPakket.php
+++ app/controllers/VoedselPakket.php
@@ -23,7 +23,8 @@ class VoedselPakket extends Controller
                         <td>$value->AantalKinderen</td>
                         <td>$value->AantalBaby</td>
                         <td><a href='" . URLROOT . "/VoedselPakket/pakketperpersoon/$value->KlantId'><i class='bx bx-box'></i></i></a></td>
-                        <td><a href='" . URLROOT . "/VoedselPakket/allproduct/$value->KlantId'><i class='bx bx-box'></i></i></a></td>
+                        <td><a href='" . URLROOT . "/VoedselPakket/allproduct/$value->KlantId'><i class='bx bx-edit-alt'></i></i></a></td>
+                        <td><a href='" . URLROOT . "/VoedselPakket/delete/$value->pakketId'><i class='bx bx-message-x'></i></i></a></td>
                       </tr>";
 
         }
@@ -66,4 +67,85 @@ class VoedselPakket extends Controller
 
 
     }
+
+    public function allproduct($klantId)
+    {
+
+        $getallproducts = $this->VoedselPakketModel->getallproducts($klantId);
+
+
+        $rows = '';
+
+        foreach ($getallproducts as $value) {
+
+            $rows .= "<tr>
+                        <td>$value->KlantNaam</td>
+                        <td>$value->Naam</td>  
+                        <td>$value->Aantal</td>
+                        <td>$value->Naam</td>
+                        <td><a href='" . URLROOT . "/VoedselPakket/update/$value->ProductId'><i class='bx bx-edit-alt'></i></i></a></td>
+                        
+                      </tr>";
+
+        }
+
+
+        $data = [
+        'rows' => $rows
+          ];
+
+        $this->view('VoedselPakket/allproduct', $data);
+
+
+    }
+    public function update($ProductId = NULL)
+      {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if ($POST['Aantal'] < 0) {
+            // Handle the error message when 'Aantal' is below 0
+            echo "Je kan geen negatief aantal invoeren";
+            return;
+        }
+
+    
+        $update = $this->VoedselPakketModel->update($POST);
+        
+        // Add a header redirect after the update using URLROOT
+        header("Refresh: 4; URL=" . URLROOT . "/VoedselPakket/pakketperpersoon". $POST['klantId']);
+        echo "Het aantal is aangepast";
+        exit;
+    } else {
+        $getvoedselpakket = $this->VoedselPakketModel->getvoedselpakketbyIds($ProductId);
+
+
+        $data = [
+            'pakketId' => $getvoedselpakket->pakketId,
+            'klantId' => $getvoedselpakket->klantId,
+            'productId' => $getvoedselpakket->productId,
+            'Aantal' => $getvoedselpakket->Aantal,
+        ];
+        $this->view('VoedselPakket/update', $data);
+    }
 }
+
+    public function delete($Id) {
+    $this->VoedselPakketModel->delete($Id);
+
+    $data =[
+      'deleteStatus' => "De Pakket is succesvol verwijderd!"
+    ];
+    $this->view("VoedselPakket/delete", $data);
+    header("Refresh:3; url=" . URLROOT . "/familie/index");
+  }
+    
+}
+
+
+
+    // Add a header redirect
+
+
+
+
