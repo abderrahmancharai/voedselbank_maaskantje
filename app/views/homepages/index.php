2b52dfa2655860e7c1f8f61345d3e07c1b4c85a4
KaganAkdeniz <kaganakdeniz2004@gmail.com>
2023-06-20 15:20:32 +0200

    index

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -1,5 +1,31 @@
 <?php require APPROOT . '/views/includes/head.php'; ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+
+<h1>welkom mederwerker</h1>
+
+
+<a href="<?=URLROOT;?>/VoedselPakket/index">Klanten Voedselpakket</a> | 
+
+<a href="wwww.jsaidnas"></a>
+
+<style>
+    .bx {
+        font-size: 2rem;
+        cursor: pointer;
+    }
+  
+</style>
+
 
 
 
